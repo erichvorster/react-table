27a62216971519172a46bf162a1125262a187c9f
erichvorster <erichvorster1@gmail.com>
2022-06-02 18:10:14 +0200

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./mockd-data.json";
 
-function App() {
+const App = () => {
+  const [contacts, setContacts] = useState(data);
+
+  console.log(contacts);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>PhoneNumber</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+              <tr>
+                <td scope="row">{contact.fullName}</td>
+                <td>{contact.address}t</td>
+                <td>{contact.phoneNumber}</td>
+                <td>{contact.email}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <h2> Add a contact</h2>
+      <form>
+        <input></input>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
